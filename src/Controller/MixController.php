7ba61e7f4ce6d4133a62035c5f68333b1a23566b
petrero <petrero@gmail.com>
2022-10-13 11:34:06 +0300

    8.2. Persisting to the Database
      Ok, let's go create a VinylMix() object! Do that with $mix = new VinylMix()... and then we can start setting data on it! Let's create a mix of one of my absolute favorite artists as a kid. I'll quickly set some other properties... we need to set, at the very least, all of the properties that have required columns in the database. For trackCount, how about some randomness for fun. And, for votes, the same thing... including negative votes... though the Internet would never be so cruel as to downvote any of my mixes that much. Finally, dd($mix).
    
      So far, this has nothing to do with Doctrine. We're just creating an object and setting data onto it. This data is hard-coded, but you can imagine replacing this with whatever the user just submitted via a form. Regardless of where we get the data, when we refresh... we have an object with data on it. Cool!
    
      Services vs Entities
      By the way, our entity class, VinylMix, is the first class we've created that is not a service. There are generally two types of classes. First, there are service objects, like TalkToMeCommand or the MixRepository we created in the last tutorial. These objects do work... but they don't hold any data besides maybe some basic config. And we always fetch services from the container, usually via autowiring. We never instantiate them directly.
    
      The second type of classes are data classes like VinylMix. The primary job of these classes is to hold data. They don't usually do any work except maybe some basic data manipulation. And unlike services, we don't fetch these objects from the container. Instead, we create them manually wherever and whenever we need them, like we just did!

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\VinylMix;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,6 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MixController extends AbstractController {
 	#[Route('/mix/new')]
 	public function new(): Response {
-		dd('new mix');
+		$mix = new VinylMix();
+		$mix->setTitle('Do you Remember... Phil Collins?!');
+		$mix->setDescription('A pure mix of drummers turned singers!');
+		$mix->setGenre('pop');
+		$mix->setTrackCount(rand(5, 20));
+		$mix->setVotes(rand(-50, 50));
+		dd($mix);
 	}
 }
