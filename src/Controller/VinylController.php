502d24d9e7172d758cbe99d42f906ac0fbc2c821
petrero <petrero@gmail.com>
2022-10-13 16:06:42 +0300

    10.5. Custom Entity Methods & Twig Magic
      Cleanup: Deleting the Old Repository
      - Ok one last tiny cleanup thing. We no longer need this MixRepository service, which loads mixes from GitHub. Let's delete it so I don't get confused... since its name is so similar to the new VinylMixRepository. Right click on MixRepository.php, go to "Refactor", and click on "Safe Delete".
    
      Easy! But... we might still be using that somewhere, right? If you go to your terminal and run:
    
        git grep MixRepository
      that'll show you where it's still being mentioned.
    
      Though, Symfony's service container is so smart, it will often tell us if we've messed something up, like if we're still using a service that doesn't exist. Watch. Try refreshing any page. Yup!
    
        Cannot autowire service App\Command\TalkToMeCommand: argument $mixRepository of method __construct() has type App\Service\MixRepository.
    
      Even though this page doesn't even use the TalkToMeCommand class, it figured out that there's a problem with it. Open it up: src/Command/TalkToMeCommand.php. Yep! We were using MixRepository... so that we could call its findAll() method. Change that to use VinylMixRepository... and then we can remove the use statement on top. The VinylMixRepository still has a findAll() method, so this will still work. This isn't a very efficient way to find a random mix, but it's good enough for now.
    
      Ok, close that class and go refresh again. The service container found another problem spot in VinylController! Head over there and... up in the constructor... yep! We're autowiring it here too. But... we're not even using the property anymore, so remove it. Also delete its use statement and a couple of other use statements that are not being... uh... used anymore more.
    
      And now... the site works again!
    
      Next, let's learn how to build custom queries via the query builder!

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
-use App\Service\MixRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +10,7 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController {
 	public function __construct(
-		private bool          $isDebug,
-		private MixRepository $mixRepository
+		private bool $isDebug,
 	) {
 	}
 
