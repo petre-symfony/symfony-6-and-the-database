5e9fdbe442db1c3137935e26c3a3c25cbd284db9
petrero <petrero@gmail.com>
2022-10-13 11:23:34 +0300

    8.1. Persisting to the Database
      Now that we have an entity class and corresponding table, we're ready to save some stuff! So... how do we insert rows into the table? Wrong question! We're only going to focus on creating objects and saving them. Doctrine will handle the insert queries for us.
    
      To help do this in the simplest way possible, let's make a fake "new Vinyl Mix" page.
    
      In the src/Controller/ directory, create a new MixController class and make this extend the normal AbstractController. Perfect! Inside, add a public function called new() that will return a Response from HttpFoundation. To make this a page, above, use the #[Route] attribute, hit "tab" to autocomplete that and let's call the URL /mix/new. Finally, to see if this is working, dd('new mix').
    
      In the real world, this page might render a form. Then, when we submit that form, we would take its data, create a VinylMix() object and save it. We'll work on stuff like that in a future tutorial. For now, let's just see if this page works. Head over to /mix/new and... got it!

---
+++ src/Controller/MixController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MixController extends AbstractController {
+	#[Route('/mix/new')]
+	public function new(): Response {
+		dd('new mix');
+	}
+}
