766a7498f566557604f4e91a5daba647a3888fdb
petrero <petrero@gmail.com>
2022-10-14 19:52:18 +0300

    18.3 Clean URLs with Sluggable
      Adding the Sluggable Attribute
      - At this point, we've activated the sluggable listener and added a slug column. But we're still missing a step. I'll prove it by going to /mix/new and... error:
    
        [...] column "slug" of relation "vinyl_mix" violates not-null constraint.
    
      Yup! Nothing is setting the slug property yet. To tell the extensions library that this is a slug property that it should set automatically, we need to add - surprise - an attribute! It's called #[Slug]. Hit "tab" to autocomplete that, which will add the use statement that you need on top. Then, say fields, which is set to an array, and inside, just title.
    
      This says:
    
        use the "title" field to generate this slug.
    
      And now... it looks like it's working! If we check the database...
    
        symfony console doctrine:query:sql 'SELECT * FROM vinyl_mix'
      Woohoo! The slug is down here... and you can see the library is also smart enough to add a little -1, -2, -3 to keep it unique.

--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\VinylMixRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Slug;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: VinylMixRepository::class)]
@@ -32,6 +33,7 @@ class VinylMix {
 	private int $votes = 0;
 
 	#[ORM\Column(length: 100, unique: true)]
+	#[Slug(fields: ['title'])]
 	private ?string $slug = null;
 
 	public function getTitle(): ?string {
